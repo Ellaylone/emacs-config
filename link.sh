bc98f68e0234c95aa8a3aa5b01668a1074857e3d
ellaylone <heamik91@yandex.ru>
2016-07-30 02:26:37 +0300

    better ask function

--- link.sh
+++ link.sh
@@ -30,26 +30,64 @@ checkEmacsDirPathInput () {
     fi
 }
 
-# Check if input values are correct
-checkIfCorrect () {
+# https://gist.github.com/davejamesmiller/1965569
+ask() {
+    # http://djm.me/ask
     while true; do
-        read -p "Is it correct(Y/n)?" yn
-        case $yn in
-            [Yy]* ) make install; break;;
-            [Nn]* ) exit;;
-            * ) echo "Please answer yes or no.";;
+
+        if [ "${2:-}" = "Y" ]; then
+            prompt="Y/n"
+            default=Y
+        elif [ "${2:-}" = "N" ]; then
+            prompt="y/N"
+            default=N
+        else
+            prompt="y/n"
+            default=
+        fi
+
+        # Ask the question (not using "read -p" as it uses stderr not stdout)
+        echo -n "$1 [$prompt] "
+
+        # Read the answer (use /dev/tty in case stdin is redirected from somewhere else)
+        read REPLY </dev/tty
+
+        # Default?
+        if [ -z "$REPLY" ]; then
+            REPLY=$default
+        fi
+
+        # Check if the reply is valid
+        case "$REPLY" in
+            Y*|y*) return 0 ;;
+            N*|n*) return 1 ;;
         esac
+
     done
 }
 
-main () {
+# Check if input values are correct
+checkIfCorrect () {
+    echo $emacs_path
+    echo $emacs_dir_path
+    if ask "Is this correct?" Y; then
+        echo "Finished"
+    else
+        exit
+    fi
+}
+
+userInput () {
     askEmacsPath
     checkEmacsPathInput
     askEmacsDirPath
     checkEmacsDirPathInput
-    echo $emacs_path
-    echo $emacs_dir_path
     checkIfCorrect
+}
+
+main () {
+    userInput
+    exit 0
 }
 
 main
