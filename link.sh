534f4396c4d143742841c69f8dfe99bcdbef873b
ellaylone <heamik91@yandex.ru>
2016-07-31 02:13:03 +0300

    backup & linking for deploy

--- link.sh
+++ link.sh
@@ -1,33 +1,25 @@
 #!/bin/bash
 
-# Ask for path to emacs config file
-# Defaults to ~/.emacs
-emacs_path="~/.emacs"
-askEmacsPath () {
-    read -e -p "Enter path to .emacs($emacs_path): " path
+home={$PWD}
+home_emacs="$home/.emacs"
+home_emacs_modules="$home/modules/"
+emacs_path="/Users/user/.emacs"
+emacs_dir_path="/Users/user/.emacs.d/"
+emacs_modules_path=""
+emacs_elpa_path=""
+
+# Ask for path
+askPath () {
+    read -e -p "$1($2): " path
 }
 
-# Ask for path to emacs user directory
-# Defaults to ~/.emacs.d/
-emacs_dir_path="~/.emacs.d/"
-askEmacsDirPath () {
-    read -e -p "Enter path to user-emacs-directory($emacs_dir_path): " path
-}
-
-# Check if emacs input is empty
-checkEmacsPathInput () {
+# Check if input is empty
+checkPathInput () {
     if [[ "$path" != '' ]]
     then
-        emacs_path=$path
+        set -- $path
     fi
-}
-
-# Check if emacs user directory input is empty
-checkEmacsDirPathInput () {
-    if [[ "$path" != '' ]]
-    then
-        emacs_dir_path=$path
-    fi
+    echo $1
 }
 
 # https://gist.github.com/davejamesmiller/1965569
@@ -69,24 +61,96 @@ ask() {
 # Check if input values are correct
 checkIfCorrect () {
     echo $emacs_path
-    echo $emacs_dir_path
-    if ask "Is this correct?" Y; then
-        echo "Finished"
-    else
+    echo $emacs_modules_path
+    echo $emacs_elpa_path
+    confirm_value= ask "Is this correct?" Y
+    if [[ $confirm_value == 1 ]]; then
         exit
     fi
 }
 
+# Check if file exists
+checkFileExists () {
+    if [ -f "$1" ]
+    then
+        return 0
+    else
+        return 1
+    fi
+}
+
+# Check if directory exists
+checkDirectoryExists () {
+    if [ -d "$1" ]
+    then
+        return 0
+    else
+        return 1
+    fi
+}
+
+backupEmacs () {
+    echo "Backup .emacs..."
+    mv $emacs_path "$emacs_path.bu"
+}
+
+backupEmacsModules () {
+    echo "Backup emacs modules..."
+    mv -r $emacs_modules_path "$emacs_modules_path.bu"
+}
+
+linkEmacs () {
+    echo "Linking emacs..."
+    ln -s $home_emacs $emacs_path
+}
+
+linkEmacsModules () {
+    echo "Linking emacs modules..."
+    ln -s $home_emacs_modules $emacs_modules_path
+}
+
+createElpa () {
+    echo "Creating Elpa..."
+    mkdir -p $emacs_elpa_path
+}
+
 userInput () {
-    askEmacsPath
-    checkEmacsPathInput
-    askEmacsDirPath
-    checkEmacsDirPathInput
+    askPath "Enter path to .emacs" $emacs_path
+    emacs_path=$(checkPathInput $emacs_path)
+    askPath "Enter path to user-emacs-directory" $emacs_dir_path
+    emacs_dir_path=$(checkPathInput $emacs_dir_path)
+    emacs_modules_path="$emacs_dir_path/modules"
+    emacs_modules_path=${emacs_modules_path//\/\//\/}
+    emacs_elpa_path="$emacs_dir_path/elpa"
+    emacs_elpa_path=${emacs_elpa_path//\/\//\/}
     checkIfCorrect
+}
+
+checkFiles () {
+    if checkFileExists $emacs_path
+    then
+        backupEmacs
+    fi
+    linkEmacs
+
+    if checkDirectoryExists $emacs_modules_path
+    then
+        backupEmacsModules
+    fi
+    linkEmacsModules
+
+    if checkDirectoryExists $emacs_elpa_path
+    then
+        echo "Elpa already exists."
+    else
+        createElpa
+    fi
 }
 
 main () {
     userInput
+    checkFiles
+    echo "Linking done."
     exit 0
 }
 
